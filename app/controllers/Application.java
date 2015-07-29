51bb86dae364db2eed6b3651c3816bdeab932ac9
aaasangwan <abhisheksangwan1994@gmail.com>
2015-07-29 20:10:12 +0530

    initial commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+
+    public Result index() {
+        return ok("Hello world");
+    }
+
+    public Result hello(String name) {
+        return ok(views.html.hello.render(name));
+    }
+
+    
+
+}
